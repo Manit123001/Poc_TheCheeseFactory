69ee8569dd1ba90c3edd6f313ada0973092b6e2d
Manit123001 <captain_new@hotmail.com>
2017-11-17 22:33:13 +0700

    AsyncTaskLoader

--- LabThead/app/src/main/java/com/mcnew/labthead/MainActivity.java
+++ LabThead/app/src/main/java/com/mcnew/labthead/MainActivity.java
@@ -1,14 +1,20 @@
 package com.mcnew.labthead;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.Looper;
 import android.os.Message;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Object> {
 
     int counter;
     TextView tvCounter;
@@ -19,6 +25,9 @@ public class MainActivity extends AppCompatActivity {
     HandlerThread backgroundHandlerThread;
     Handler backgrounHandler;
     Handler mainHandler;
+
+    SampleAsyncTask sampleAsyncTask;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -116,6 +125,7 @@ public class MainActivity extends AppCompatActivity {
         /**
          * Thread Method 4: HandlerThread (2 3 combined)
          **/
+        /*-
         backgroundHandlerThread = new HandlerThread("BackgrondHandlerThread");
         backgroundHandlerThread.start();
 
@@ -150,14 +160,136 @@ public class MainActivity extends AppCompatActivity {
         Message msgBack = new Message();
         msgBack.arg1 = 0;// Start count at 0
         backgrounHandler.sendMessageDelayed(msgBack, 1000);
-        
+        -*/
+
+        /**
+         * Thread Method 5: AsyncTask
+         **/
+        //sampleAsyncTask = new SampleAsyncTask();
+        //sampleAsyncTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, 0, 100);
+//        sampleAsyncTask.execute(0, 100); // Not use
+
+
+        /**
+         * Thread Method 6: AsyncTaskLoader
+         **/
+        getSupportLoaderManager().initLoader(1, null, this);
     }
+
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
 
         //thread.interrupt();
-        backgroundHandlerThread.quit();
+        // backgroundHandlerThread.quit();
+
+        // sampleAsyncTask.cancel(true);
+    }
+
+    @Override
+    public Loader<Object> onCreateLoader(int id, Bundle args) {
+        if (id == 1) {
+            return new AdderAsyncTaskLoader(MainActivity.this, 5, 11);
+        }
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Object> loader, Object data) {
+        Log.d("LLLL", "onLoadFinished");
+        if (loader.getId() == 1) {
+            Integer resule = (Integer) data;
+            tvCounter.setText(resule + "");
+        }
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Object> loader) {
+
+    }
+
+    // do restorestate(do with licycle. in activity)
+    static class AdderAsyncTaskLoader extends AsyncTaskLoader<Object> {
+
+        int a, b;
+
+        Integer result;
+
+        public AdderAsyncTaskLoader(Context context, int a, int b) {
+            super(context);
+            this.a = a;
+            this.b = b;
+        }
+
+        @Override
+        protected void onStartLoading() {
+            super.onStartLoading();
+            Log.d("LLLL", "onStartLoading");
+
+            if(result != null){
+                deliverResult(result);
+            }
+            forceLoad(); // called loadInBackground are working.
+
+        }
+
+        @Override
+        public Integer loadInBackground() {
+
+            Log.d("LLLL", "loadInBackground");
+            // Background Thread
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            result = a + b;
+            return result;
+        }
+
+        @Override
+        protected void onStopLoading() {
+            super.onStopLoading();
+            Log.d("LLLL", "onStopLoading");
+
+
+        }
+    }
+
+    class SampleAsyncTask extends AsyncTask<Integer, Float, Boolean> {
+        @Override
+        protected Boolean doInBackground(Integer... integers) {
+            // Run in BackgroundThread
+            int start = integers[0]; // 0
+            int end = integers[1]; // 100
+
+            for (int i = start; i < end; i++) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    return false;
+                }
+                publishProgress(i + 0.0f);
+            }
+            return true;
+        }
+
+        @Override
+        protected void onProgressUpdate(Float... values) {
+            // Run on Main Thread
+            super.onProgressUpdate(values);
+            float progress = values[0];
+            tvCounter.setText(progress + "%");
+        }
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            // Run on Main Thread
+            super.onPostExecute(aBoolean);
+            // Work with aBoolean
+
+        }
     }
 }
